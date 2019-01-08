a03952b57db78cf8e30fa072bc14626d150fa75b
Obrazcoff <obrazcoff@gmail.com>
2019-01-08 01:52:30 +0100

    Add custom task adding

--- src/components/TaskAddForm/TaskAddForm.js
+++ src/components/TaskAddForm/TaskAddForm.js
@@ -3,17 +3,32 @@ import React, { Component } from 'react';
 import './TaskAddForm.css';
 
 export default class TaskAddForm extends Component {
-  state = {};
+  state = {
+    newTaskText: '',
+  };
+
+  onTextChange = e => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.onTaskAdded(this.state.text);
+  };
+
   render() {
     return (
-      <div className="task-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onTaskAdded('new task')}
-        >
-          Add task
-        </button>
-      </div>
+      <form className="task-add-form d-flex" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          onChange={this.onTextChange}
+          placeholder="What to do next?"
+        />
+        <button className="btn btn-outline-secondary">Add task</button>
+      </form>
     );
   }
 }
